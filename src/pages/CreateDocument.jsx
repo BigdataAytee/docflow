8e92fca8df7f970b97c34815fd52495cb909539c
Ayoola Taiwo Ayodeji <taiwoayodeji1995@gmail.com>
2026-06-19 18:51:10 +0000

    Visual edits

--- src/pages/CreateDocument.jsx
+++ src/pages/CreateDocument.jsx
@@ -1297,7 +1297,7 @@ export default function CreateDocument() {
                 <FileDown className="h-4 w-4 mr-1" />
                 Preview &amp; Download PDF
               </Button>
-              <Button variant="ghost" className="w-full" onClick={() => setShowPdfPreview(true)}>
+              <Button variant="ghost" className="w-full hidden" onClick={() => setShowPdfPreview(true)}>
                 <Upload className="h-4 w-4 mr-1" />
                 Share PDF
               </Button>
